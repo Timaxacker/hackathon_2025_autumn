ad57e2527f3dfa80cfead5555e35e3125067d8de
Timaxacker <laztimofey@gmail.com>
2025-10-28 13:40:47 +0300

    add id for Student

--- Student.py
+++ Student.py
@@ -1,5 +1,6 @@
 class Student():
-    def __init__(self, surname, mark1, mark2):
+    def __init__(self, id, surname, mark1, mark2):
+        self.id = id
         self.surname = surname
         self.mark1 = mark1
         self.mark2 = mark2
